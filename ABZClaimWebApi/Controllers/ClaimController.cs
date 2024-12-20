0d4248e27efd6ddb6659e85c9ef4df22e4047e64
sruthi <sruthibadineni@gmail.com>
2024-12-20 23:36:12 +0530

    Added7

--- ABZClaimWebApi/Controllers/ClaimController.cs
+++ ABZClaimWebApi/Controllers/ClaimController.cs
@@ -95,7 +95,7 @@ namespace ABZClaimWebApi.Controllers
                 await claimRepo.InsertPolicyAsync(policy);
                 // HttpClient client = new HttpClient();
                 //await client.PostAsJsonAsync("http://localhost:5189/api/Claim", new { PolicyNo = policy.PolicyNo });
-                return Created($"api/Claim/{policy.PolicyNo}", policy);
+                return Created();
             }
             catch (Exception ex)
             {

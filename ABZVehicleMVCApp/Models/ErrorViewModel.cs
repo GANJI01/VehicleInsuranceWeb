fce93e5c7a9fbe515a8b0a9fa0c3768346ff9070
chandana295 <90788711+chandana295@users.noreply.github.com>
2024-12-20 11:15:51 +0530

    Delete ABZVehicleMVCApp directory

--- ABZVehicleMVCApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ABZVehicleMVCApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

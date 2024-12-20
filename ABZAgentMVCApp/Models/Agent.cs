d333eaaf30ca21f9febc0cd81bf3dc86496942be
GANJI01 <115651184+GANJI01@users.noreply.github.com>
2024-12-20 11:08:52 +0530

    models added

--- ABZAgentMVCApp/Models/Agent.cs
+++ ABZAgentMVCApp/Models/Agent.cs
@@ -2,5 +2,14 @@
 {
     public class Agent
     {
+        public string AgentID { get; set; } = null!;
+
+        public string AgentName { get; set; } = null!;
+
+        public string AgentPhone { get; set; } = null!;
+
+        public string AgentEmail { get; set; } = null!;
+
+        public string LicenseCode { get; set; } = null!;
     }
 }
--- ABZAgentMVCApp/Models/Policy.cs
+++ ABZAgentMVCApp/Models/Policy.cs
@@ -2,5 +2,22 @@
 {
     public class Policy
     {
+        public string PolicyNo { get; set; } = null!;
+
+        public string? PrososalNo { get; set; }
+
+        public decimal? NoClaimBonus { get; set; }
+
+        public string ReceiptNo { get; set; } = null!;
+
+        public DateTime ReceiptDate { get; set; }
+
+        public string PaymentMode { get; set; } = null!;
+
+        public decimal? Amount { get; set; }
+
+        public virtual ICollection<PolicyAddon> PolicyAddons { get; set; } = new List<PolicyAddon>();
+
+        public virtual Proposal? PrososalNoNavigation { get; set; }
     }
 }

eb56e5d596bb8e6f5a6df0cfdf12722fb87501a9
sruthi <sruthibadineni@gmail.com>
2024-12-27 09:56:11 +0530

    customerQueryclass added

---
+++ ABZCustomerQueryLibrary/Repos/EFCustomerQueryRepoAsync.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABZCustomerQueryLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABZCustomerQueryLibrary.Repos
+{
+    public class EFCustomerQueryRepoAsync:ICustomerQueryRepoAsync
+    {
+        ABZQueryDBContext ctx=new ABZQueryDBContext();
+
+        public async Task DeleteCustomerQueryAsync(string queryId)
+        {
+            CustomerQuery customerQuery = await GetCustomerQueryAsync(queryId);
+            ctx.CustomerQueries.Remove(customerQuery);
+            await ctx.SaveChangesAsync();
+
+        }
+
+        public async Task<List<CustomerQuery>> GetAllCustomerQueriesAsync()
+        {
+            List<CustomerQuery> customerQueries = await ctx.CustomerQueries.ToListAsync();
+            return customerQueries;
+        }
+
+        public async Task<CustomerQuery> GetCustomerQueryAsync(string queryId)
+        {
+            try
+            {
+                CustomerQuery customerQuery = await (from cq in ctx.CustomerQueries where queryId == cq.QueryID select cq).FirstAsync();
+                return customerQuery;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No such queryId exist");
+            }
+        }
+
+        public async Task<List<CustomerQuery>> GetCustomerQueryByCustomerAsync(string customerId)
+        {
+            List<CustomerQuery> customerQueries= await(from cq in ctx.CustomerQueries where customerId==cq.CustomerID select cq).ToListAsync();
+            if(customerQueries.Count == 0)
+            {
+                throw new Exception("No Such CustomerId exist");
+            }
+            else
+            {
+                return customerQueries;
+            }
+        }
+
+        public async Task InsertCustomerAsync(Customer customer)
+        {
+            await ctx.Customers.AddAsync(customer);
+            await ctx.SaveChangesAsync();
+        }
+
+        public async Task InsertCustomerQueryAsync(CustomerQuery customerquery)
+        {
+            await ctx.CustomerQueries.AddAsync(customerquery);
+            await ctx.SaveChangesAsync();
+        }
+
+        public async Task UpdateCustomerQueryAsync(string queryId, CustomerQuery customerquery)
+        {
+            CustomerQuery customerQuery1=await GetCustomerQueryAsync(queryId);
+            customerQuery1.Description= customerquery.Description;
+            customerQuery1.QueryDate=customerquery.QueryDate;
+            customerQuery1.Status=customerquery.Status;
+        }
+    }
+}
---
+++ ABZCustomerQueryWebApi/Controllers/CustomerQueryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ABZCustomerQueryLibrary.Models;
+using ABZCustomerQueryLibrary.Repos;
+
+namespace ABZCustomerQueryWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerQueryController : ControllerBase
+    {
+        ICustomerQueryRepoAsync cusqueRepo;
+        public CustomerQueryController(ICustomerQueryRepoAsync repo)
+        {
+            cusqueRepo = repo;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            List<CustomerQuery> customerQueries=await cusqueRepo.GetAllCustomerQueriesAsync();
+            return Ok(customerQueries);
+        }
+        [HttpGet("{queryId}")]
+        public async Task<ActionResult> GetOne(string queryId)
+        {
+            try
+            {
+                CustomerQuery customerQuery = await cusqueRepo.GetCustomerQueryAsync(queryId);
+                return Ok(customerQuery);
+
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpGet("ByCustomer/{customerId}")]
+        public async Task<ActionResult> GetByCustomer(string customerId)
+        {
+            try
+            {
+                List<CustomerQuery> customerQueries=await cusqueRepo.GetCustomerQueryByCustomerAsync(customerId);
+                return Ok(customerQueries);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        
+
+    }
+}

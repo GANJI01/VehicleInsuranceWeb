02333073f5d435a93deff66b3726a9b139db229e
sruthi <sruthibadineni@gmail.com>
2024-12-28 17:49:23 +0530

    Changes in models

--- ABZVehicleInsuranceMVCAPP/Models/ProductAddOn.cs
+++ ABZVehicleInsuranceMVCAPP/Models/ProductAddOn.cs
@@ -1,13 +1,16 @@
-﻿namespace ABZVehicleInsuranceMVCAPP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ABZVehicleInsuranceMVCAPP.Models
 {
     public class ProductAddon
     {
+        [RegularExpression(@"\w[10]", ErrorMessage = "Product Id must be 10 characters")]
         public string ProductID { get; set; } = null!;
-
+        [RegularExpression(@"\w[10]", ErrorMessage = "Addon Id must be 10 characters")]
         public string AddonID { get; set; } = null!;
-
+        [Required]
         public string AddonTitle { get; set; } = null!;
-
+        [Required]
         public string AddonDescription { get; set; } = null!;
 
        // public virtual Product? Product { get; set; } = null!;
